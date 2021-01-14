567cbe517c6c1484e267aebdd71f22488b10d19e
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-14 12:54:56 -0500

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -1,31 +1,3 @@
 import React from 'react';
-
-
-
-describe("login tests", ()=>{
-    test("test 1", ()=> {
-        console.log("running our first test");
-    });
-    
-    test("test 2", ()=> {
-        console.log("running our first test");
-    });
-    
-    test("test 3", ()=> {
-        console.log("running our first test");
-    });
-});
-
-describe("click tests", ()=>{
-    test("test 1", ()=> {
-        console.log("running our first test");
-    });
-    
-    test("test 2", ()=> {
-        console.log("running our first test");
-    });
-    
-    test("test 3", ()=> {
-        console.log("running our first test");
-    });
-});
+import { render, screen } from '@testing-library/react';
+import App from './App';
