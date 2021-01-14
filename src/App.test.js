6ed77d9ee74ec1b934c463fa8cd1aae63dfaf3e1
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-14 12:50:25 -0500

    lecture work-in-progress

---
+++ src/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+test("test 1", ()=> {
+    console.log("running our first test");
+});
+
+test("test 2", ()=> {
+    console.log("running our first test");
+});
+
+test("test 3", ()=> {
+    console.log("running our first test");
+});
---
+++ src/components/AnimalForm.test.js
@@ -0,0 +1,12 @@
+
+test("test 1", ()=>{
+    console.log("test complete for animal form");
+});
+
+test("test 1", ()=>{
+    console.log("test complete for animal form");
+});
+
+test("test 1", ()=>{
+    console.log("test complete for animal form");
+});
