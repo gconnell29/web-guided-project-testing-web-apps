7975332ef522b3dd3c900966a1780df469a700b0
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-14 13:17:04 -0500

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -8,14 +8,14 @@ test("renders App without errors", ()=>{
 
 test("renders App header", ()=> {
     //Arrange: render our App component
-    const appInterface = render(<App />);
+    render(<App />);
 
     console.log(appInterface);
 
     //Act: get our header DOM
     // const header = screen.queryByText("Add New Animal");
     // const header = screen.queryByTestId("header");
-    // const header = screen.getByText(/add new animal/i);
+    const header = screen.getByText(/add new animal/i);
 
     //Assert: Make sure that the header actually exists
 });
