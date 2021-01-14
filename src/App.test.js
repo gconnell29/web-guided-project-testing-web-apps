8e6402aa24e14652893d6a4419cf5d83b9eb3040
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-14 13:21:04 -0500

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -16,5 +16,6 @@ test("renders App header", ()=> {
     let header = screen.queryByText(/add new animal/i);
 
     //Assert: Make sure that the header actually exists
-    expect(false);
+    const value = true;
+    expect(value).toBeTruthy();
 });
