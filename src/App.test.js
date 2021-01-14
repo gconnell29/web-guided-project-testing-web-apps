2a00879687a05799724ae4b5894ce475d6275522
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-14 12:54:08 -0500

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -2,14 +2,30 @@ import React from 'react';
 
 
 
-it("test 1", ()=> {
-    console.log("running our first test");
+describe("login tests", ()=>{
+    test("test 1", ()=> {
+        console.log("running our first test");
+    });
+    
+    test("test 2", ()=> {
+        console.log("running our first test");
+    });
+    
+    test("test 3", ()=> {
+        console.log("running our first test");
+    });
 });
 
-test("test 2", ()=> {
-    console.log("running our first test");
-});
-
-test("test 3", ()=> {
-    console.log("running our first test");
+describe("click tests", ()=>{
+    test("test 1", ()=> {
+        console.log("running our first test");
+    });
+    
+    test("test 2", ()=> {
+        console.log("running our first test");
+    });
+    
+    test("test 3", ()=> {
+        console.log("running our first test");
+    });
 });
