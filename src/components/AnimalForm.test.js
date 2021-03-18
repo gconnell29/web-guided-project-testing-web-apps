7fd495cd635d1efe4edf4dfee02b89251fedf43a
kirkby <kirkby@neighbor.com>
2021-03-17 21:17:28 -0600

    progress

--- src/components/AnimalForm.test.js
+++ src/components/AnimalForm.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import AnimalForm from './AnimalForm';
 
 test("AnimalForm renders", () => {
@@ -9,5 +10,13 @@ test("AnimalForm renders", () => {
 test("AnimalForm renders error message when there are errors", () => {
   render(<AnimalForm errors={["this is an error"]} />)
   screen.getByText(/there are errors/i)
+});
+
+test("AnimalForm adds an animal when form is filled and submitted", () => {
+  render(<AnimalForm errors={[]} />);
+
+  const speciesInput = screen.getByLabelText(/species/i);
+
+
 })
 
