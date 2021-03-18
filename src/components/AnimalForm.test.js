22936a2b81d33a40d3686be0826930f60f264db4
kirkby <kirkby@neighbor.com>
2021-03-17 21:28:49 -0600

    progress

--- src/components/AnimalForm.test.js
+++ src/components/AnimalForm.test.js
@@ -8,7 +8,7 @@ test("AnimalForm renders", () => {
 })
 
 test("AnimalForm renders error message when there are errors", () => {
-  render(<AnimalForm errors={["this is an error"]} />)
+  render(<AnimalForm />)// errors={["this is an error"]} />)
   screen.getByText(/there are errors/i)
 });
 
